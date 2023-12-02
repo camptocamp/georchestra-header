278b0d8b0fab53ea64946f417b8ecc202c45e761
marwanehcine <marwane.benhcine@camptocamp.com>
2023-12-02 15:48:28 +0100

    adding alert message for user when password will expire

--- src/header.ce.vue
+++ src/header.ce.vue
@@ -30,6 +30,8 @@ const state = reactive({
 
 const isAnonymous = computed(() => !state.user || state.user.anonymous)
 const isAdmin = computed(() => state.user?.adminRoles?.admin)
+const isWarned = computed(() => state.user?.warned)
+const remainingDays = computed(() => state.user?.remainingDays)
 const adminRoles = computed(() => state.user?.adminRoles)
 
 const loginUrl = computed(() => {
@@ -49,7 +51,6 @@ onMounted(() => {
     'eng'
   getUserDetails().then(user => {
     state.user = user
-
     if (user?.adminRoles?.admin) {
       getPlatformInfos().then(
         platformInfos => (state.platformInfos = platformInfos)
@@ -168,6 +169,7 @@ onMounted(() => {
               </li>
             </ul>
           </div>
+          <span class="text-gray-400" v-if="isWarned"> {{ t('remaining_days_msg_part1') }} {{remainingDays}} {{ t('remaining_days_msg_part2') }} <a href="console/account/changePassword">{{ t('remaining_days_msg_part3') }}</a></span>
         </nav>
       </div>
       <div></div>
