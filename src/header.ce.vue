13381c44bd6d6058f96dc0ba08e8e1ad698c25c5
Florian Necas <florian.necas@camptocamp.com>
2025-09-17 10:45:24 +0200

    feat: add stylesheet to chat bubble

--- src/header.ce.vue
+++ src/header.ce.vue
@@ -246,6 +246,7 @@ onMounted(() => {
     <chat-bubble
       v-if="chatbotEndpoint"
       :endpoint="chatbotEndpoint"
+      :stylesheet="state.config.stylesheet"
     ></chat-bubble>
   </header>
 </template>
